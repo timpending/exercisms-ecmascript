d8cc6fecad3fd01b115136ddd1b659978689498d
Tim Pender <mrtimpender@gmail.com>
2016-11-06 20:51:19 -0700

    completed hamming

---
+++ hamming/hamming.js
@@ -0,0 +1,16 @@
+class Hamming {
+  compute(str1, str2){
+    if (str1.length !== str2.length){
+      throw new Error('DNA strands must be of equal length.')
+    }
+    let count=0
+    for (let i=0; i<str1.length; i++){
+      if (str1.charAt(i)!== str2.charAt(i)){
+        count++
+      }
+    }
+    return count
+  }
+}
+
+export default Hamming;
--- leap/leap.js
+++ leap/leap.js
@@ -1,8 +1,1 @@
-/**
- * @param  {number} year
- * Numeric year.
- *
- * @return {boolean}
- * Whether given year is a leap year.
- */
-export default year => year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+export default (year) => year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)
