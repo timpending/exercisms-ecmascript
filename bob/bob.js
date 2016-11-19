2d90de2b2f3429ee98427713f9fbc7dab41806c7
Tim Pender <mrtimpender@gmail.com>
2016-11-19 12:11:00 -0700

    bob done

--- bob/bob.js
+++ bob/bob.js
@@ -3,13 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
+let question = (input) => input.endsWith('?');
+let silence = (input) => input.replace(/\s+/g, '') === '';
+let yelling = (input) => input.toUpperCase() === input && input.toLowerCase() !== input;
+
 class Bob {
   hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (yelling(message)) {
+      return 'Whoa, chill out!'
+    } else if (question(message)) {
+      return 'Sure.';
+    }
+    else if (silence(message)){
+      return 'Fine. Be that way!';
+    } else {
+    return 'Whatever.';
+    }
   }
 }
 
 export default Bob;
-
